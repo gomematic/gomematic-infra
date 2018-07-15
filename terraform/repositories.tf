cbe95096139f948158f87af7ac9b120e64f4f2e6
Thomas Boerger <thomas@webhippie.de>
2018-07-15 22:22:01 +0200

    Integrate github management (#1)
    * Integrate github management
    * Fix drone conditions

---
+++ terraform/repositories.tf
@@ -0,0 +1,179 @@
+resource "github_repository" "gomematic-android" {
+  name               = "gomematic-android"
+  description        = "Gomematic: Android client"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-api" {
+  name               = "gomematic-api"
+  description        = "Gomematic: API server"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-cli" {
+  name               = "gomematic-cli"
+  description        = "Gomematic: CLI client"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-desktop" {
+  name               = "gomematic-desktop"
+  description        = "Gomematic: Desktop client"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-docs" {
+  name               = "gomematic-docs"
+  description        = "Gomematic: Documentation"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-go" {
+  name               = "gomematic-go"
+  description        = "Gomematic: SDK for Go"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-infra" {
+  name               = "gomematic-infra"
+  description        = "Gomematic: Infrastructure"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-ios" {
+  name               = "gomematic-ios"
+  description        = "Gomematic: iOS client"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-js" {
+  name               = "gomematic-js"
+  description        = "Gomematic: SDK for Javascript"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-php" {
+  name               = "gomematic-php"
+  description        = "Gomematic: SDK for PHP"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-python" {
+  name               = "gomematic-python"
+  description        = "Gomematic: SDK for Python"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-ruby" {
+  name               = "gomematic-ruby"
+  description        = "Gomematic: SDK for Ruby"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-scripts" {
+  name               = "gomematic-scripts"
+  description        = "Gomematic: Scripting"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "gomematic-ui" {
+  name               = "gomematic-ui"
+  description        = "Gomematic: Web UI"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "homebrew-gomematic" {
+  name               = "homebrew-gomematic"
+  description        = "Gomematic: Homebrew"
+  homepage_url       = "https://gomematic.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,3 +11,81 @@ variable "cloudflare_domain" {
   type    = "string"
   default = "gomematic.tech"
 }
+
+variable "github_token" {
+  type = "string"
+}
+
+variable "github_org" {
+  type    = "string"
+  default = "gomematic"
+}
+
+variable "github_members" {
+  type = "map"
+
+  default = {
+    gomematicz = "admin"
+    tboerger   = "admin"
+  }
+}
+
+variable "bot_members" {
+  type = "map"
+
+  default = {
+    gomematicz = "maintainer"
+  }
+}
+
+variable "bot_repos" {
+  type = "map"
+
+  default = {
+    gomematic-android  = "admin"
+    gomematic-api      = "admin"
+    gomematic-cli      = "admin"
+    gomematic-desktop  = "admin"
+    gomematic-docs     = "admin"
+    gomematic-go       = "admin"
+    gomematic-infra    = "admin"
+    gomematic-ios      = "admin"
+    gomematic-js       = "admin"
+    gomematic-php      = "admin"
+    gomematic-python   = "admin"
+    gomematic-ruby     = "admin"
+    gomematic-scripts  = "admin"
+    gomematic-ui       = "admin"
+    homebrew-gomematic = "admin"
+  }
+}
+
+variable "maintainer_members" {
+  type = "map"
+
+  default = {
+    tboerger = "maintainer"
+  }
+}
+
+variable "maintainer_repos" {
+  type = "map"
+
+  default = {
+    gomematic-android  = "push"
+    gomematic-api      = "push"
+    gomematic-cli      = "push"
+    gomematic-desktop  = "push"
+    gomematic-docs     = "push"
+    gomematic-go       = "push"
+    gomematic-infra    = "push"
+    gomematic-ios      = "push"
+    gomematic-js       = "push"
+    gomematic-php      = "push"
+    gomematic-python   = "push"
+    gomematic-ruby     = "push"
+    gomematic-scripts  = "push"
+    gomematic-ui       = "push"
+    homebrew-gomematic = "push"
+  }
+}
